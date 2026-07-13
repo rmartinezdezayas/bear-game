b4a5cecc1d70abf2c1b0a161a9a227a6273cda8a
Roberto <rmartinezdezayas@gmail.com>
2026-07-14 01:29:24 +0200

    added some details

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 50.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -250.0
+const CROUCH_VELOCITY_MULTIPLIER = 0.5
 
+@onready var animation_tree : AnimationTree = $AnimationTree
+var state_machine
+
+func _ready() -> void:
+	state_machine = animation_tree["parameters/playback"]
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +17,46 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
+	# Use the crouch multiplier when crouched
+	var crouch_multiplier = CROUCH_VELOCITY_MULTIPLIER if Input.is_action_pressed("crouch") else 1
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * crouch_multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * crouch_multiplier)
 
 	move_and_slide()
+	animations()
+
+func animations():
+	# 1. Flip player sprite logic
+	var is_moving = (Input.is_action_pressed("right") or Input.is_action_pressed("left")) and (!Input.is_action_pressed("right") or !Input.is_action_pressed("left"))
+	if Input.is_action_pressed("right"):
+		$Sprite2D.flip_h = false
+	elif Input.is_action_pressed("left"):
+		$Sprite2D.flip_h = true
+	
+	# 2. Air/Jump state logic (Highest Priority)
+	if not is_on_floor():
+		if velocity.y < 0:
+			state_machine.travel("jump-max")
+		else:
+			state_machine.travel("fall")
+			
+	# 3. Ground state logic (Only runs when is_on_floor() is true)
+	else:
+		if Input.is_action_pressed("crouch"):
+			if is_moving:
+				state_machine.travel("crouch-walk")
+			else:
+				state_machine.travel("crouch-idle")
+		else:
+			if is_moving:
+				state_machine.travel("run")
+			else:
+				state_machine.travel("idle")
